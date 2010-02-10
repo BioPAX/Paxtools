e3b8ef1fb9d5665b2cce40fa4f144869f2ee3ac1
Emek <>
2010-02-10 18:34:00 -0500

    Added missing property filter

---
+++ paxtools-core/src/main/java/org/biopax/paxtools/controller/PropertyFilter.java
@@ -0,0 +1,14 @@
+package org.biopax.paxtools.controller;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Emek
+ * Date: Feb 10, 2010
+ * Time: 6:32:57 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface PropertyFilter
+{
+
+    boolean filter(PropertyEditor editor);
+}
