c9751338255c03d01107e9f064d635d088466686
Emek Demir <demir@cbio.mskcc.org>
2010-04-05 18:06:03 -0400

    A simple merger for merging already ID normalized models
    A test model iterator for getting models for testing purposes

---
+++ paxtools-core/src/main/java/org/biopax/paxtools/controller/SimpleMerger.java
@@ -0,0 +1,111 @@
+package org.biopax.paxtools.controller;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.biopax.paxtools.model.BioPAXElement;
+import org.biopax.paxtools.model.Model;
+
+import java.util.*;
+
+/**
+ * Utility class to merge multiple biopax models into one. Note that this merger does not preserve
+ * the integrity of the passed models. Target will be a merged model and source will become
+ * unusable.
+ */
+
+public class SimpleMerger
+{
+// ------------------------------ FIELDS ------------------------------
+
+	private static final Log log = LogFactory.getLog(SimpleMerger.class);
+
+	private final EditorMap map;
+
+
+	// --------------------------- CONSTRUCTORS ---------------------------
+
+	/**
+	 * @param map a class to editor map containing the editors for the elements of models to be
+	 *            modified.
+	 */
+	public SimpleMerger(EditorMap map)
+	{
+		this.map = map;
+	}
+
+// ------------------------ INTERFACE METHODS ------------------------
+
+	// --------------------- Interface Visitor ---------------------
+
+// -------------------------- OTHER METHODS --------------------------
+
+
+	/**
+	 * Merges the <em>source</em> model into <em>target</em> model.
+	 *
+	 * @param target model into which merging process will be done
+	 * @param source model that is going to be merged with <em>target</em>
+	 */
+	public void merge
+			(Model target, Model source)
+	{
+
+		Set<BioPAXElement> sourceElements = source.getObjects();
+		for (BioPAXElement bpe : sourceElements)
+		{
+			BioPAXElement paxElement = target.getByID(bpe.getRDFId());
+			if (paxElement == null)
+			{
+				target.add(bpe);
+			}
+
+		}
+
+		for (BioPAXElement bpe : sourceElements)
+		{
+			updateObjectFields(bpe, target);
+		}
+	}
+
+	/**
+	 * Updates each value of <em>existing</em> element, using the value(s) of <em>update</em>.
+	 *
+	 * @param update BioPAX element of which values are ued for update
+	 */
+	private void updateObjectFields(BioPAXElement update, Model target)
+	{
+		Set<PropertyEditor> editors =
+				map.getEditorsOf(update);
+		for (PropertyEditor editor : editors)
+		{
+			if (editor instanceof ObjectPropertyEditor)
+			{
+				if(editor.isMultipleCardinality())
+				{
+					Set<BioPAXElement> values = (Set<BioPAXElement>) editor.getValueFromBean(update);
+					for (BioPAXElement value : values)
+					{
+						migrateToPC(update, target, editor, value);
+					}
+				}
+				else
+				{
+				BioPAXElement value = (BioPAXElement) editor.getValueFromBean(update);
+					migrateToPC(update, target, editor, value);
+				}
+			}
+		}
+
+	}
+
+	private void migrateToPC(BioPAXElement update, Model target, PropertyEditor editor,
+	                         BioPAXElement value)
+	{
+		if (!target.contains(value))
+		{
+			BioPAXElement newValue = target.getByID(value.getRDFId());
+			editor.setPropertyToBean(update, newValue);
+		}
+	}
+
+}
---
+++ paxtools-simple-io/src/test/java/org/biopax/paxtools/io/simpleIO/TestModelIterator.java
@@ -0,0 +1,77 @@
+package org.biopax.paxtools.io.simpleIO;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.biopax.paxtools.model.BioPAXLevel;
+import org.biopax.paxtools.model.Model;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.Iterator;
+
+/**
+ * TODO:Class description User: demir Date: Mar 25, 2010 Time: 2:31:16 PM
+ */
+public class TestModelIterator implements Iterator<Model>
+{
+	private Log log = LogFactory.getLog(TestModelIterator.class);
+	private Iterator<String> fileNames;
+	private String dir;
+	private SimpleReader simpleReader;
+
+	public TestModelIterator(BioPAXLevel level, String pathRoot) throws URISyntaxException
+	{
+		this(level,
+			pathRoot,
+			new FilenameFilter()
+				{
+				    public boolean accept(File dir, String name)
+				    {
+				        return (name.endsWith(".owl"));
+				    }
+				}
+		);
+
+	}
+
+
+	public TestModelIterator(BioPAXLevel level,  String pathRoot, FilenameFilter filter)
+			throws URISyntaxException
+	{
+		dir = pathRoot+File.separator+level.name();
+
+		System.out.println("dir = " + dir);
+
+		File testDir = new File(getClass()
+				.getClassLoader().getResource(dir).toURI());
+
+		System.out.println("testDir) = " + testDir);
+		System.out.println(Arrays.toString(testDir.listFiles()));
+
+		fileNames = Arrays.asList(testDir.list(filter)).iterator();
+		
+		simpleReader = new SimpleReader(level);
+	}
+
+	public boolean hasNext()
+	{
+		return fileNames.hasNext();
+	}
+
+	public Model next()
+	{
+		String s = fileNames.next();
+		System.out.println("Reading "+s);
+		InputStream in = getClass().getClassLoader() // - at the root classpath
+			     	.getResourceAsStream(dir + File.separator + s);
+		return simpleReader.convertFromOWL(in);
+	}
+
+	public void remove()
+	{
+		throw new UnsupportedOperationException();
+	}
+}
