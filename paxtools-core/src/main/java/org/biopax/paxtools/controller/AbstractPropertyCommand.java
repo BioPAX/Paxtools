276618b1bbc1130202ea41474b59e4a4de6e7719
Emek Demir <demir@cbio.mskcc.org>
2010-09-07 15:29:59 -0400

    New undo/redo facility .. with the files added..:)

---
+++ paxtools-core/src/main/java/org/biopax/paxtools/controller/AbstractPropertyCommand.java
@@ -0,0 +1,57 @@
+package org.biopax.paxtools.controller;
+
+import org.biopax.paxtools.model.BioPAXElement;
+
+/**
+ */
+public abstract class AbstractPropertyCommand implements Command
+{
+	BioPAXElement bpe;
+	PropertyEditor editor;
+	Object value;
+
+	AbstractPropertyCommand(BioPAXElement bpe, PropertyEditor editor, Object value)
+	{
+
+		if (bpe == null || editor == null)
+		{
+			throw new IllegalArgumentException();
+		}
+		this.bpe = bpe;
+		this.editor = editor;
+		this.value=value;
+
+	}
+
+	public boolean undo()
+	{
+		try
+		{
+			undoAction();
+			return true;
+		}
+		catch (Exception e)
+		{
+			return false;
+		}
+	}
+
+	protected abstract void undoAction();
+
+
+	public boolean redo()
+	{
+		try
+		{
+			redoAction();
+			return true;
+		}
+		catch (Exception e)
+		{
+			return false;
+		}
+	}
+
+	protected abstract void redoAction();
+
+}
---
+++ paxtools-core/src/main/java/org/biopax/paxtools/controller/PropertyRemoveCommand.java
@@ -0,0 +1,65 @@
+package org.biopax.paxtools.controller;
+
+import org.biopax.paxtools.model.BioPAXElement;
+
+import javax.swing.undo.CannotRedoException;
+import javax.swing.undo.CannotUndoException;
+
+public class PropertyRemoveCommand extends AbstractPropertyCommand
+{
+
+
+	PropertyRemoveCommand(BioPAXElement bpe, PropertyEditor editor, Object value)
+	{
+		super(bpe, editor, value);
+		if (!editor.isMultipleCardinality())
+		{
+			throw new IllegalArgumentException();
+		}
+	}
+
+
+	@Override
+	protected void undoAction()
+	{
+		editor.setValueToBean(value, bpe);
+	}
+
+
+	@Override
+	protected void redoAction()
+	{
+		editor.removeValueFromBean(value, bpe);
+
+	}
+
+	public boolean canUndo()
+	{
+		return bpe != null;
+	}
+
+	public boolean canRedo()
+	{
+		return bpe != null && value != null;
+	}
+
+	public boolean isSignificant()
+	{
+		return value != null;
+	}
+
+	public String getPresentationName()
+	{
+		return "Remove " + value + " from " + editor.getProperty() + "s of " + bpe;
+	}
+
+	public String getUndoPresentationName()
+	{
+		return "Add" + value + "to" + editor.getProperty() + "s of " + bpe;
+	}
+
+	public String getRedoPresentationName()
+	{
+		return "Re-Remove " + value + " from " + editor.getProperty() + "s of " + bpe;
+	}
+}
