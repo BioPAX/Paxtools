728665f733ad3079e33ce93134f6e71a32111366
Igor Rodchenkov <rodche@gmail.com>
2011-05-04 11:06:17 -0400

    Bug fixing patch - applied to previous revision (preparing to the release and dev. version change...)

--- paxtools-core/src/main/java/org/biopax/paxtools/model/level3/NucleicAcidReference.java
+++ paxtools-core/src/main/java/org/biopax/paxtools/model/level3/NucleicAcidReference.java
@@ -1,6 +1,8 @@
 package org.biopax.paxtools.model.level3;
 
 
+import java.util.Set;
+
 /**
  * Role interface for NucleicAcidReferences, namely DNA and RNA. They share the common behaviour of having "regions"
  * on them.
@@ -8,5 +10,9 @@ package org.biopax.paxtools.model.level3;
  */
 public interface NucleicAcidReference extends SequenceEntityReference
 {
+   Set<NucleicAcidRegionReference> getSubRegion();
 
+	void addSubRegion(NucleicAcidRegionReference regionReference);
+
+	void removeSubRegion(NucleicAcidRegionReference regionReference);
 }
