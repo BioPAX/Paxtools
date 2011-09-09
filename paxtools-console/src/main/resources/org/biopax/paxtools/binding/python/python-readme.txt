41df78d1a9dcc34ec2212df8dff107fada0a4e5b
Igor Rodchenkov <rodche@gmail.com>
2011-09-09 10:42:07 -0400

    Edited the Python/JPype example and readme.

--- paxtools-console/src/main/resources/org/biopax/paxtools/binding/python/python-readme.txt
+++ paxtools-console/src/main/resources/org/biopax/paxtools/binding/python/python-readme.txt
@@ -1,13 +1,14 @@
 #May.19.2009, questions to Gary Bader (gary.bader@utoronto.ca)
-# TODO: update this information for the paxtools2!
+#Updated in September 2011
 
-1. JPype can be used to control Java Paxtools from Python.
-Download and install JPype from http://jpype.sourceforge.net/
-This code was tested with JPype 0.5.4.1 and Python 2.5.1, Java 1.5 on OS X
-and Pythonb 2.6, Java 1.6 on Windows XP, but it seems that JPype is quite
-stable and should work with multiple versions of Java, Python and
-different OSes
+JPype can be used to control Java Paxtools from Python.
 
+The example script worked on JPype 0.5.4.1 and Python 2.5.1, Java 1.5 on OS X; 
+Python 2.6, Java 1.6 on Windows XP; JPype 0.5.4.2, Python 2.6, Java 1.6 on 
+OS X 10.6; but it seems that JPype is quite stable and should work with multiple
+versions of Java, Python and different OSes
+
+1. Download and install JPype from http://jpype.sourceforge.net/
 Hello world python script using JPype looks like this:
 
 from jpype import *
@@ -15,30 +16,13 @@ startJVM(getDefaultJVMPath(), "-ea")
 java.lang.System.out.println("hello world")
 shutdownJVM()
 
-2. [old paxtools] Check out and build Paxtools from sourceforge
-See instructions at http://sourceforge.net/scm/?type=cvs&group_id=85345
-cvs -d:pserver:anonymous@biopax.cvs.sourceforge.net:/cvsroot/biopax login cvs
--z3 -d:pserver:anonymous@biopax.cvs.sourceforge.net:/cvsroot/biopax co -P
-Paxtools
-
-Once downloaded, compile using ant (must have ant installed). This was tested
-using the Sun Java compiler.  Type "ant" and ensure build was successful.
-This creates the paxtools.jar file in the build/jars directory
+2. Download Paxtools
+Get either the paxtools*-no-jena.jar (smaller) or paxtools*.jar (has additional jenaIO module)
+'fat' JAR from http://sourceforge.net/projects/biopax/files/paxtools/
 
 3. Program in python using paxtools
-Set this up by copying all jar files in paxtools/lib and the paxtools.jar file
-itself to your own convenient lib directory
-
-Update: with new paxtools - you only need one 'fat' JAR (but can also use individual modules):
-get either the "paxtools*-no-jena.jar" (smaller, uses simpleIO) or "paxtools*.jar" (with jenaIO)
-from http://downloads.sourceforge.net/project/biopax/paxtools/
-
-(If you get the snapshot JARs without dependencies, set the class path appropriately for your OS 
-and lib location - use absolute paths to all jars)
-
-See pythonPaxtoolsExample.py for some example code that creates
-a BioPAX Level 3 OWL file with a few simple objects. The resulting
-OWL file from this script is included "test.owl"
+See pythonPaxtoolsExample.py example script that creates
+a simple BioPAX Level 3 OWL file ("test.owl").
 
 Notes:
 -If JPype gives an error that a class is not callable, it usually means
--- paxtools-console/src/main/resources/org/biopax/paxtools/binding/python/pythonPaxtoolsExample.py
+++ paxtools-console/src/main/resources/org/biopax/paxtools/binding/python/pythonPaxtoolsExample.py
@@ -1,55 +1,92 @@
+###
+# Example using BioPAX PaxTools from Python
+# 
+# PaxTools: http://www.biopax.org/paxtools, 
+#           http://sourceforge.net/projects/biopax/files/paxtools/
+# JPype:    http://jpype.sourceforge.net/
+# BioPAX:   http://www.biopax.org
+#
+###
+
 from jpype import *
+
 #call this to initialize use of Java
-#(with using a 'fat' paxtools.jar (with all dependencies built-in), setting long java.class.path is not required anymore - )
-#startJVM(getDefaultJVMPath(), "-ea","-Djava.class.path=$PAXTOOLS_HOME/lib/collections-generic-4.01.jar:$PAXTOOLS_HOME/lib/commons-logging.jar:$PAXTOOLS_HOME/lib/concurrent.jar:$PAXTOOLS_HOME/lib/icu4j_3_4.jar:$PAXTOOLS_HOME/lib/iri.jar:$PAXTOOLS_HOME/lib/jakarta-oro.jar:$PAXTOOLS_HOME/lib/jena.jar:$PAXTOOLS_HOME/lib/junit-4.1.jar:$PAXTOOLS_HOME/lib/log4j-1.2.12.jar:$PAXTOOLS_HOME/lib/paxtools.jar:$PAXTOOLS_HOME/lib/xercesImpl.jar:$PAXTOOLS_HOME/lib/xml-apis.jar")
-startJVM(getDefaultJVMPath(), "-ea","-Djava.class.path=paxtools.jar")
+startJVM(getDefaultJVMPath(), "-ea", "-Xmx1g", "-Djava.class.path=paxtools.jar")
+
 #print out using java or python
 java.lang.System.out.println("Starting pythonPaxToolsExample")
 print java.lang.System.getProperty("java.version")
 print java.lang.System.getProperty("java.class.path")
-#get the paxtools package as a shortcut
+
+#get the paxtools root package as a shortcut
 paxPkg = JPackage("org.biopax.paxtools")
-#short form e.g. Document = JPackage('org').w3c.dom.Document
-#create a new BioPAX model to add data to
-l3Factory = paxPkg.impl.level3.Level3FactoryImpl()
+
+#create a new BioPAX L3 factory
+#l3Factory = paxPkg.impl.level3.Level3FactoryImpl()
+l3Factory = paxPkg.model.BioPAXLevel.L3.getDefaultFactory()
+
+#create a new empty BioPAX model
 model = l3Factory.createModel()
 
-#will be using the following BioPAX classes (model interfaces):
+#(highly recommended) use an xml base (URI prefix for elements we create)
+xmlBase = "http://biopax.org/examples/pythonPaxtools#"
+# set xml base 
+# - before paxtools v4.0.0 2011/08:
+# model.getNameSpacePrefixMap().put("", xmlBase)
+# - since paxtools v4.0.0 2011/08:
+model.setXmlBase(xmlBase)
+
+#get BioPAX classes (model interfaces); in this example: Protein, CellularLocationVocabulary, UnificationXref:
 proteinClass = java.lang.Class.forName("org.biopax.paxtools.model.level3.Protein", True, java.lang.ClassLoader.getSystemClassLoader())
 cellularLocationCvClass = java.lang.Class.forName("org.biopax.paxtools.model.level3.CellularLocationVocabulary", True, java.lang.ClassLoader.getSystemClassLoader())
+unificationXrefClass = java.lang.Class.forName("org.biopax.paxtools.model.level3.UnificationXref", True, java.lang.ClassLoader.getSystemClassLoader())
 
-#add elements to the model
-#step 1: create an object using the factory
-protein = l3Factory.create(proteinClass, "protein1")
-#step 2: must set unique RDF ID and add to model for each object created
+#create/add a couple of elements to the model
+#step 1: create a simple protein state using the factory method and unique identifier (URI)
+#        (in a real-world scenario, we would also add at least a protein reference with one unification xref)
+
+protein = l3Factory.create(proteinClass, xmlBase + "protein1")
+protein.addComment("python: created " + protein.getRDFId())
+
+#NOTE: despite xmlBase was set for the entire model, you still have to use 
+#      absolute URIs when creating your BioPAX objects; i.e., use this URI prefix 
+#      when creating an Entity class object. However, do not necessarily use your
+#      own xml base and id when creating a UtilityClass object (such as ProteinReference 
+#      or ControlledVocabulary) for which a better absolute URI might be found (at identifiers.org).
+
+#step 2: add to the model
 model.add(protein)
-#step 3: add data to your object
+
+#step 3: set some properties
 protein.addAvailability("availability text")
-cellLoc = l3Factory.create(cellularLocationCvClass, "cellularLocationVocabulary1")
+cellLoc = l3Factory.create(cellularLocationCvClass, "http://identifiers.org/obo.go/GO:0005737")
 model.add(cellLoc)
-cellLoc.addComment("comment")
+cellLoc.addComment("python: created " + cellLoc.getRDFId())
 cellLoc.addTerm("cytoplasm")
 protein.setCellularLocation(cellLoc)
 
-#or, do the creation, setting RDF ID and adding to model in one step
-proteinClass = java.lang.Class.forName("org.biopax.paxtools.model.level3.Protein", True, java.lang.ClassLoader.getSystemClassLoader())
-#once you get a reference to a Class object of a given type, you can reuse it across multiple calls to model.addNew method
-protein2 = model.addNew(proteinClass, "protein2")
-protein2.addComment("created protein2")
+#alternatively, one can create, set the id (URI), and add the element in one step
+protein2 = model.addNew(proteinClass, xmlBase + "protein2")
+protein2.addComment("created " + protein2.getRDFId())
+# let's add a unification xref to the CV
+ux = model.addNew(unificationXrefClass, xmlBase + "XREF_GO_0005737")
+ux.setDb("GO")
+ux.setId("GO:0005737")
+cellLoc.addXref(ux)
 
-#export to BioPAX OWL
+#export the model to a BioPAX OWL file
 javaIO = JPackage("java.io")
 io = paxPkg.io.SimpleIOHandler(paxPkg.model.BioPAXLevel.L3)
 fileOS = javaIO.FileOutputStream("test.owl")
-#output to stdout
-io.convertToOWL(model, java.lang.System.out)
-#output to a file
 io.convertToOWL(model, fileOS)
 fileOS.close()
 
-#read from file
+#import a BioPAX model from the file
 fileIS = javaIO.FileInputStream("test.owl")
 model2 = io.convertFromOWL(fileIS)
+fileIS.close()
+
+#output to console
 io.convertToOWL(model, java.lang.System.out)
 
 #end use of jpype - docs say you can only do this once, so all java must be run before calling this
