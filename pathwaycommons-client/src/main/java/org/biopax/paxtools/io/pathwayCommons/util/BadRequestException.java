5ae252f71bfc7031d2b07393a6a57844eefc2530
B. Arman Aksoy <arman@cbio.mskcc.org>
2012-01-19 17:20:20 -0500

    Removed pathwaycommons-client module files for good. It now lives in the cpath google code repository: http://code.google.com/p/pathway-commons/

--- pathwaycommons-client/src/main/java/org/biopax/paxtools/io/pathwayCommons/util/BadRequestException.java
+++
@@ -1,9 +0,0 @@
-package org.biopax.paxtools.io.pathwayCommons.util;
-
-import cpath.service.jaxb.ErrorResponse;
-
-public class BadRequestException extends PathwayCommonsException {
-    public BadRequestException(ErrorResponse error) {
-        super(error);
-    }
-}
