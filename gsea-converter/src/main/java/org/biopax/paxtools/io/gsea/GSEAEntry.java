17333b3968634ce2f02be14657a785154733ff12
Igor Rodchenkov <rodche@gmail.com>
2012-10-22 22:18:40 -0400

    Polished how the GSEA exporter/converter prints taxonomy ids...

--- gsea-converter/src/main/java/org/biopax/paxtools/io/gsea/GSEAEntry.java
+++ gsea-converter/src/main/java/org/biopax/paxtools/io/gsea/GSEAEntry.java
@@ -61,10 +61,16 @@ class GSEAEntry {
     	StringBuilder toReturn = new StringBuilder();
     	
     	if (!identifiers.isEmpty()) {
-    		String tax = ((taxID.isEmpty()) ? "unknown" : taxID);
-    		toReturn.append(tax).append(" (taxonomy); ").append(name)
-    			.append("\t").append(description)
+    		String tax = ((taxID.isEmpty()) ? "unspecified" : taxID);
+    		toReturn 
+    		// the (unique) 'name' column comes first
+    			.append(tax).append(": ").append(name)
+    			.append("\t")
+    		// next, comes the description column
+    			.append(description)
+    			.append("; organism: ").append(tax)
     			.append("; id type: ").append(idType);
+    		// finally, - all data (identifiers) columns
     		for (String id : identifiers) {
     			toReturn.append("\t").append(id);
     		}
