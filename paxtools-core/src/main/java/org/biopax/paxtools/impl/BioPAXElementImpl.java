4972881aecfdac3fed944fe7d4a71ea63e55b480
rodche <rodche@gmail.com>
2013-09-26 16:37:47 -0400

    Bug fixed: moved MessageDigest instance creation inside the method (removed static final MessageDigest MD5_DIGEST) - java.security.MessageDigest is not thread-safe and, e.g., it threw "…BioPAXFactory - Could not instantiate BioPAX Type:…" errors caused by ArrayIndexOutOfBoundsException exception in the cPath2/Tomcat server when there were several graph queries coming within a second)

--- paxtools-core/src/main/java/org/biopax/paxtools/impl/BioPAXElementImpl.java
+++ paxtools-core/src/main/java/org/biopax/paxtools/impl/BioPAXElementImpl.java
@@ -172,18 +172,6 @@ public abstract class BioPAXElementImpl implements BioPAXElement
 	}
     
 
-    //to calculate the PK from URI
-    public static final MessageDigest MD5_DIGEST; 
-
-	static {
-		try {
-			MD5_DIGEST = MessageDigest.getInstance("MD5");
-		} catch (NoSuchAlgorithmException e) {
-			throw new RuntimeException("Cannot instantiate MD5 MessageDigest!", e);
-		}
-	}
-    
-
 	/**
 	 * Utility method that is called once per object
 	 * to generate the primary key {@link #getPk()}
@@ -194,12 +182,23 @@ public abstract class BioPAXElementImpl implements BioPAXElement
 	 * @return
 	 */
 	private static String md5hex(String id) {
-		byte[] digest = MD5_DIGEST.digest(id.getBytes());
+		
+		// MessageDigest is not thread-safe and cheap to build 
+		// - so we create a instance every time here:
+		MessageDigest md5; 
+		try {
+			md5 = MessageDigest.getInstance("MD5");
+		} catch (NoSuchAlgorithmException e) {
+			throw new RuntimeException("Cannot instantiate MD5 MessageDigest!", e);
+		}
+		
+		byte[] digest = md5.digest(id.getBytes());
+		
 		StringBuffer sb = new StringBuffer();
 		for (byte b : digest)
 			sb.append(Integer.toHexString((int) (b & 0xff) | 0x100).substring(1, 3));
-		String hex = sb.toString();
-		return hex;
+		
+		return sb.toString();
 	}
     
 	
