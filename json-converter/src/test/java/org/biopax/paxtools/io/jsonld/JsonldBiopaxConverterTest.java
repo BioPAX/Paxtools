66e012b463a1541fe97ba23dc35e180200ac42e0
IgorRodchenkov <rodche@gmail.com>
2024-05-12 14:16:01 -0400

    Followed up with #54 - added tests and reverted Jena dependency back to v3 (due to weird jsonld conversion issues when using jena 4 or 5).

--- json-converter/src/test/java/org/biopax/paxtools/io/jsonld/JsonldBiopaxConverterTest.java
+++ json-converter/src/test/java/org/biopax/paxtools/io/jsonld/JsonldBiopaxConverterTest.java
@@ -29,18 +29,38 @@ public class JsonldBiopaxConverterTest {
 	final void test2() throws IOException {
 		JsonldConverter converter = new JsonldBiopaxConverter();
 		// convert owl test file in resource directory to jsonld format
-		InputStream in = getClass().getResourceAsStream("/pc14-test.owl");
+		InputStream in = getClass().getResourceAsStream("/demo-pathway-xsd.owl");
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		converter.convertToJsonld(in, baos);
 		String res = baos.toString("UTF-8");
 		Assertions.assertAll(
 				() -> Assertions.assertThrows(IllegalArgumentException.class, () -> URI.create("http://")), //bad URI
-				() -> Assertions.assertDoesNotThrow(() -> URI.create("bioregistry.io/chebi:18367")), //valid URI but not good for LD (LinkedData)
-				() -> Assertions.assertDoesNotThrow(() -> URI.create("chebi:18367")), // valid URI (CURIE)
-				() -> Assertions.assertDoesNotThrow(() -> URI.create("http://bioregistry.io/chebi:18367")), //good valid absolute URI
-				() -> Assertions.assertTrue(res.contains("@id\": \"http://bioregistry.io/chebi:18367")),
-				() -> Assertions.assertTrue(res.contains("@id\": \"http://bioregistry.io/mi:0361")),//as long as it has 'http://' (valid abs. uri w/o schema would fail here due Jena bug)
-				() -> Assertions.assertTrue(res.contains("@id\": \"chebi:18367")) //unchanged
+				() -> Assertions.assertDoesNotThrow(() -> URI.create("bioregistry.io/chebi:20")), //valid URI but not good for LD (LinkedData)
+				() -> Assertions.assertDoesNotThrow(() -> URI.create("chebi:20")), // valid URI (CURIE)
+				() -> Assertions.assertDoesNotThrow(() -> URI.create("http://bioregistry.io/chebi:20")), //good valid absolute URI
+				() -> Assertions.assertTrue(res.contains("@id\" : \"http://bioregistry.io/chebi:20")),
+				() -> Assertions.assertTrue(res.contains("@id\" : \"http://bioregistry.io/mi:0361")),//as long as it has 'http://' (valid abs. uri w/o schema would fail here due Jena bug)
+				() -> Assertions.assertTrue(res.contains("@id\" : \"chebi:20")), //unchanged
+				() -> Assertions.assertTrue(res.contains("\"@id\" : \"http://www.biopax.org/release/biopax-level3.owl#displayName\""))
+		);
+	}
+
+	@Test
+	final void test3() throws IOException {
+		JsonldConverter converter = new JsonldBiopaxConverter();
+		// convert owl test file in resource directory to jsonld format
+		InputStream in = getClass().getResourceAsStream("/demo-pathway-noxsd.owl");
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		converter.convertToJsonld(in, baos);
+		String res = baos.toString("UTF-8");
+		Assertions.assertAll(
+				() -> Assertions.assertThrows(IllegalArgumentException.class, () -> URI.create("http://")),
+				() -> Assertions.assertDoesNotThrow(() -> URI.create("bioregistry.io/chebi:20")),
+				() -> Assertions.assertDoesNotThrow(() -> URI.create("chebi:20")),
+				() -> Assertions.assertDoesNotThrow(() -> URI.create("http://bioregistry.io/chebi:28")),
+				() -> Assertions.assertTrue(res.contains("@id\" : \"http://bioregistry.io/chebi:28")),
+				() -> Assertions.assertTrue(res.contains("\"@id\" : \"http://www.biopax.org/release/biopax-level3.owl#displayName\"")),
+				() -> Assertions.assertTrue(res.contains("@id\" : \"chebi:28"))
 		);
 	}
 
